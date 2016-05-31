bc552d1afa288b7d161ead30044c4167aede56ab
milicaz <zm025555@gmail.com>
2016-05-31 17:18:48 +0200

    Ubacivanje istih formi.

---
+++ PKI/src/actions/OkAction.java
@@ -0,0 +1,26 @@
+package actions;
+
+import java.awt.event.ActionEvent;
+
+import javax.swing.AbstractAction;
+import javax.swing.JDialog;
+
+import forms.GenerateKeyPairForm;
+
+public class OkAction extends AbstractAction {
+	
+	private JDialog standardForm;
+	
+	public OkAction(JDialog standardForm){
+		this.standardForm = standardForm;
+	}
+
+	public void actionPerformed(ActionEvent arg0) {
+		// TODO Auto-generated method stub
+
+		
+	}
+	
+	
+
+}
